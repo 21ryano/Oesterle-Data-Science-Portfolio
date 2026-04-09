206d002d75fe7f195e0f121ab9014682d1661996
Ryan Oesterle <roester2@nd.edu>
2026-04-09 18:43:31 -0400

    Update MLStreamlitApp.py

--- MLStreamlitApp/MLStreamlitApp.py
+++ MLStreamlitApp/MLStreamlitApp.py
@@ -484,8 +484,6 @@ if df is not None and target and features and target not in features:
                 fontsize=10
             )
             st.pyplot(fig)
-
-        st.subheader("Model Performance")
         
         # Classification models (Decision Tree, KNN ONLY)
         if is_classification and model_name != "Logistic Regression":
